9ddb4599e7d3760d517898ab40a70d4d3112159d
IbarraOrtizDev <ortizalexander2244@gmail.com>
2025-02-27 22:12:29 -0500

    fixed yml

--- setup.py
+++ setup.py
@@ -8,19 +8,18 @@ setup(
     install_requires=[
         'requests',
         'pandas',
-        'openpyxl',  # Necesario para exportar a Excel
-        'sqlite3'    # sqlite3 es parte de la biblioteca estándar de Python, no es necesario instalarlo
+        'openpyxl'
     ],
     entry_points={
         'console_scripts': [
-            'ingestion=ingestion:main',  # Asumiendo que tienes una función main en ingestion.py
+            'ingestion=ingestion:main',
         ],
     },
     include_package_data=True,
     description='Proyecto para la extracción, almacenamiento y verificación de datos desde un API utilizando Python y SQLite',
-    author='[Tu Nombre]',
-    author_email='[Tu Email]',
-    url='https://github.com/[tu_usuario]/[nombre_repositorio]',
+    author='Edwin Alexander Ibarra - Sergio Rios',
+    author_email='edwin.ibarra@est.iudigital.edu.co - sergio.rios@est.iudigital.edu.co',
+    url='https://github.com/IbarraOrtizDev/control-exhibicion-front',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
