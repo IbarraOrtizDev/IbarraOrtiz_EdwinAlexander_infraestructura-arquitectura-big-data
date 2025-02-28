1934cf1835a1ddf7c53bc8f2b29b5f3cf3878c0d
IbarraOrtizDev <ortizalexander2244@gmail.com>
2025-02-27 22:51:00 -0500

    complete process

--- src/ingestion.py
+++ src/ingestion.py
@@ -2,50 +2,31 @@ import requests
 import sqlite3
 import pandas as pd
 
+from manageDB import ManageDB
+
 def fetch_data_from_api(api_url, params=None):
     response = requests.get(api_url, params=params)
     response.raise_for_status()  # Raise an exception for HTTP errors
     return response.json()
 
-def create_database(db_path):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS data (
-            id INTEGER PRIMARY KEY,
-            field1 TEXT,
-            field2 TEXT,
-            field3 TEXT
-        )
-    ''')
-    conn.commit()
-    return conn
-
-def insert_data_to_db(conn, data):
-    cursor = conn.cursor()
-    for item in data:
-        cursor.execute('''
-            INSERT INTO data (field1, field2, field3) VALUES (?, ?, ?)
-        ''', (item['field1'], item['field2'], item['field3']))
-    conn.commit()
-
 def generate_sample_file(data, file_path):
     df = pd.DataFrame(data)
+    print(df.value_counts())
     df.to_excel(file_path, index=False)
 
 def generate_audit_file(db_path, api_data, audit_file_path):
     conn = sqlite3.connect(db_path)
-    df_db = pd.read_sql_query('SELECT * FROM data', conn)
+    df_db = pd.read_sql_query('SELECT * FROM users', conn)
     df_api = pd.DataFrame(api_data)
     
     with open(audit_file_path, 'w') as f:
         f.write(f"Number of records in API: {len(df_api)}\n")
         f.write(f"Number of records in DB: {len(df_db)}\n")
         f.write("Data integrity check:\n")
-        f.write(df_db.equals(df_api))
+        f.write(str(df_db.equals(df_api)))
 
 def main():
-    api_url = 'https://api.example.com/data'
+    api_url = 'https://jsonplaceholder.typicode.com/todos/'
     db_path = 'src/static/db/ingestion.db'
     sample_file_path = 'src/static/xlsx/ingestion.xlsx'
     audit_file_path = 'src/static/auditoria/ingestion.txt'
@@ -53,12 +34,12 @@ def main():
     # Fetch data from API
     data = fetch_data_from_api(api_url)
 
-    # Create database and insert data
-    conn = create_database(db_path)
-    insert_data_to_db(conn, data)
-
+    print(data)
     # Generate sample file
     generate_sample_file(data, sample_file_path)
+
+    manageDB = ManageDB()
+    manageDB.insert_users_batch(data)
 
     # Generate audit file
     generate_audit_file(db_path, data, audit_file_path)
---
+++ src/manageDB.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+
+class ManageDB:
+    def __init__(self):
+        self.db = sqlite3.connect('src/static/db/ingestion.db')
+        self.cursor = self.db.cursor()
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                userId INTEGER,
+                title TEXT,
+                completed BOOLEAN
+            )
+        ''')
+        self.db.commit()
+
+    def insert_user(self, userId, id, title, completed):
+        self.cursor.execute('''
+            INSERT INTO users(userId, id, title, completed)
+            VALUES(?, ?, ?, ?)
+        ''', (userId, id, title, completed))
+        self.db.commit()
+
+    def insert_users_batch(self, users):
+        users_tuples = [(user['userId'], user['id'], user['title'], user['completed']) for user in users]
+        self.cursor.executemany('''
+            INSERT INTO users(userId, id, title, completed)
+            VALUES(?, ?, ?, ?)
+        ''', users_tuples)
+        self.db.commit()
+
+    def fetch_all_users(self):
+        self.cursor.execute('''
+            SELECT * FROM users
+        ''')
+        return self.cursor.fetchall()
+
+    def close(self):
+        self.db.close()
